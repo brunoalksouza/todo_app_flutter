a4126043753fba5a2a27bfe814661416f951e732
Bruno Souza <brunoalksouza@gmail.com>
2023-09-07 17:02:07 -0300

    creating new todos with style

--- lib/widgets/todo_list_item.dart
+++ lib/widgets/todo_list_item.dart
@@ -5,6 +5,35 @@ class TodoListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(5),
+        color: Colors.grey[200],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Text(
+                  '20/20/20',
+                  style: TextStyle(fontSize: 13),
+                ),
+                Text(
+                  'tarefaaaa',
+                  style: TextStyle(fontSize: 13),
+                ),
+              ],
+            ),
+            Text(
+              'Tarefa caralho',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
